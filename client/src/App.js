174bc8a27e5362f64c663b5bd821d67bd9874bf1
Joshua Starling <joshstarling97@gmail.com>
2019-11-08 22:30:26 -0500

    fixed error in api.js, and setup hooks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import { useLocalStorage } from
+import { useLocalStorage } from './Hooks/LocalStorage'
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const[player, setPlayer] = useLocalStorage('player', 'country');
+  const[search, setSearch] = useLocalStorage('search', 1)
+
   return (
     <div className="App">
-      
+      <h1>Women's World Cup - 2019</h1>
+      <select value={player} onChange={e => setPlayer(e.target.value)}></select>
+
+      <input type="number" placeholder="Search Number" value={search} onChange={e => setSearch(e.target.value)} />
     </div>
   );
 }
