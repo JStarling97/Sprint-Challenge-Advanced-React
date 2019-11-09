be85a252a968455236128f9dd876e44e44ee4428
Joshua Starling <joshstarling97@gmail.com>
2019-11-08 22:51:28 -0500

    fixed some more errors, and redid styling cause it broke.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { useLocalStorage } from './Hooks/LocalStorage'
+import Players from './Components/Players';
+import data from './Components/Data';
+import api from './Components/Api';
+
 import './App.css';
 
 function App(props) {
-
-  const[player, setPlayer] = useLocalStorage('player', 'country');
-  const[search, setSearch] = useLocalStorage('search', 1)
+  const [player, setPlayer] = useLocalStorage('player', 'country');
+  const [search, setSearch] = useLocalStorage('search', 1);
+  const [players] = useState([]);
 
   return (
-    <div className="App">
-      <h1>Women's World Cup - 2019</h1>
-      <select value={player} onChange={e => setPlayer(e.target.value)}></select>
-
-      <input type="number" placeholder="Search Number" value={search} onChange={e => setSearch(e.target.value)} />
-    </div>
+    <>
+      <h1>Women's World Cup - 2019 </h1>
+      <select value={player} onChange={e => setPlayer(e.target.value)}>
+      </select>
+      <input
+        type='number' placeholder='Search Count' value={search} onChange={e => setSearch(e.target.value)} />
+      <div>
+        {players.map((player, index) => (
+          <Players player={player} key={index} />
+        ))}
+      </div>
+    </>
   );
 }
-
 export default App;
--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,17 +1,21 @@
-import { useEffect } from 'react'
-import { useLocalStorage } from './useLocalStorage'
 
-export function useDarkMode(key, initalValue) {
-    const [someValue, setSomeValue] = useLocalStorage('ket', false);
+import { useEffect } from 'react';
+import { useLocalStorage } from '../Hooks/LocalStorage';
+
+function Players(key, initialValue) {
+    const [someValue, setSomeValue] = useLocalStorage('key', false);
 
     useEffect(() => {
         const body = document.querySelector('body')
-        if(someValue === true) {
+        if (someValue === true) {
             body.classList.add('dark-mode')
         } else {
             body.classList.remove('dark-mode')
         }
+
     }, [someValue])
 
     return [someValue, setSomeValue]
 }
+
+export default Players;
